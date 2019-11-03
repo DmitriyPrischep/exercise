deb85a020765bba483c6d9d8a9c9946b283adc56
Dmitriy Prishchep <dmitriy.prishchep@myoffice.team>
2019-11-03 15:41:27 +0300

    updated config file v2

--- server.py
+++ server.py
@@ -14,8 +14,7 @@ if not cfg:
 	exit('correct config expected')
 
 fileManager = fileController.FileManager()
-# with open('config.json', encoding = 'utf-8-sig') as config_file:
-#     conf = json.load(config_file)
+
 sock = socket.socket()
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(('', 80))
